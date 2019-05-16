325a38a294c78434c80c942903c637be09175281
Dakpan94 <jhouten17@gmail.com>
2019-05-16 17:01:06 +0200

    updated for new application

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -8,7 +8,7 @@ new Vue({
         },
         mounted (){
             axios
-            .get("https://r6statsstreamwidget.azurewebsites.net/api/GetGenericStats?code=oLhpAqRTdm0vH7u0OSCmPygLxUHAlc4xKpSiVJB6xTxpBxhzPZt2hg%3D%3D&platform=pc&username=efg-dakpan")
+            .get("https://r6statsapi.azurewebsites.net/api/GetTheBestSquad?platform=pc&usernames=efg-steluktal,efg-dakpan,bluecewe,ishnutella,bros10.tr")
             .then(response => (this.data = response.data))
             .catch(error => console.log(error))
             .finally(this.isLoading = false)
